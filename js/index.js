7056cca6fa208e0db4b038d861d6cdd8d98d333e
Vlad Myronenko <orurez111@gmail.com>
2023-10-16 19:12:59 +0300

    fix: add function changeBackgorundImage, move apiBaseUrl and key on the external variables

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 'use strict';
-setValue('London');
 const backImage = document.querySelector('.weather-wrapper');
 const input = document.querySelector('.side-info__input');
 const searchButton = document.querySelector('.side-info__btn');
@@ -12,6 +11,10 @@ const currentCityName = document.querySelector('.main-info__city-name');
 const currentTime = document.querySelector('.main-info__sub');
 const currentWeatherIcon = document.querySelector('.main-info__img');
 const currentWeatherText = document.querySelector('.main-info__cloudy');
+const key = '511f37ef6753431f902114219231310';
+const apiBaseUrl = 'https://api.weatherapi.com/v1';
+
+setValue('London');
 
 input.addEventListener('keydown', (e) => {
   if (e.keyCode === 13) {
@@ -31,34 +34,36 @@ defaultListCities.forEach((city) =>
 );
 
 function setValue(city) {
-  fetch(`https://api.weatherapi.com/v1/current.json?key=511f37ef6753431f902114219231310&q=${city}&aqi=no`)
+  return fetch(`${apiBaseUrl}/current.json?key=${key}&q=${city}&aqi=no`)
     .then((data) => data.json())
-    .then((object) => {
+    .then((response) => {
       input.value = '';
-      cloudyValue.textContent = object.current.cloud + '%';
-      humidityValue.textContent = object.current.humidity + '%';
-      windValue.textContent = object.current.wind_kph + 'km/h';
-      temperatureValue.innerHTML = Math.round(object.current.temp_c) + '&#176';
-      currentCityName.textContent = object.location.name;
-      currentTime.textContent = object.location.localtime;
-      currentWeatherIcon.src = object.current.condition.icon;
-      currentWeatherText.textContent = object.current.condition.text;
+      cloudyValue.textContent = response.current.cloud + '%';
+      humidityValue.textContent = response.current.humidity + '%';
+      windValue.textContent = response.current.wind_kph + 'km/h';
+      temperatureValue.innerHTML = Math.round(response.current.temp_c) + '&#176';
+      currentCityName.textContent = response.location.name;
+      currentTime.textContent = response.location.localtime;
+      currentWeatherIcon.src = response.current.condition.icon;
+      currentWeatherText.textContent = response.current.condition.text;
 
-      const weatherCode = object.current.condition.code;
-      if (weatherCode === 1000) {
-        backImage.style.backgroundImage = "url('./assets/images/sunny.jpg')";
-      } else if ((weatherCode >= 1180 && weatherCode <= 1201) || (weatherCode >= 1240 && weatherCode <= 1252) || (weatherCode >= 1273 && weatherCode <= 1276)) {
-        backImage.style.backgroundImage = "url('./assets/images/rain.jpg')";
-      } else if (weatherCode === 1003) {
-        backImage.style.backgroundImage = "url('./assets/images/cloudy-med.jpg')";
-      } else if (weatherCode >= 1006 && weatherCode <= 1171) {
-        backImage.style.backgroundImage = "url('./assets/images/cloudy.jpg')";
-      } else {
-        backImage.style.backgroundImage = "url('./assets/images/snow.jpg')";
+      function changeBackgorundImage() {
+        const weatherCode = response.current.condition.code;
+        if (weatherCode === 1000) {
+          backImage.style.backgroundImage = "url('./assets/images/sunny.jpg')";
+        } else if ((weatherCode >= 1180 && weatherCode <= 1201) || (weatherCode >= 1240 && weatherCode <= 1252) || (weatherCode >= 1273 && weatherCode <= 1276)) {
+          backImage.style.backgroundImage = "url('./assets/images/rain.jpg')";
+        } else if (weatherCode === 1003) {
+          backImage.style.backgroundImage = "url('./assets/images/cloudy-med.jpg')";
+        } else if (weatherCode >= 1006 && weatherCode <= 1171) {
+          backImage.style.backgroundImage = "url('./assets/images/cloudy.jpg')";
+        } else {
+          backImage.style.backgroundImage = "url('./assets/images/snow.jpg')";
+        }
       }
+      changeBackgorundImage();
     })
     .catch((err) => {
       alert('Проверьте. верно ли указан город');
-      return;
     });
 }
